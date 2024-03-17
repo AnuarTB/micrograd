d80698396208206e569c32da5022ab3656c2f8c7
Anuar Talipov <anuartb@gmail.com>
2024-03-17 16:26:19 +0900

    Add more arithmetic operations.
    - Add operations as div, sub and pow.
    - Allow operations with scalars.

--- micrograd/example.py
+++ micrograd/example.py
@@ -1,7 +1,18 @@
 from micrograd import Value
 
-a = Value(1)
-b = Value(2)
-
-c = a + b
+a = Value(3)
+b = Value(4)
+c = a * b
 print(c.data)
+
+d = Value(4)
+e = 2 + d
+print(e.data)
+
+f = Value(5)
+g = 3 - f
+print(g.data)
+
+h = Value(4)
+i = 9 / h
+print(i.data)
--- micrograd/micrograd.py
+++ micrograd/micrograd.py
@@ -1,5 +1,19 @@
-import queue
+import math
 from dataclasses import dataclass
+import queue
+
+def scalar_to_value(func):
+    """All Value operations can accept scalar values.
+    
+    Casts any scalar values that are used in operations with
+    Value to Value type.
+    """
+    def wrapper(*args):
+        if isinstance(args[1], (float, int)):
+            return func(args[0], Value(args[1]))
+        else:
+            return func(*args)
+    return wrapper
 
 class Value(object):
   def __init__(self, data):
@@ -12,6 +26,7 @@ class Value(object):
   def __repr__(self):
     return f'val={self.data}'
 
+  @scalar_to_value
   def __add__(self, other):
     out = Value(self.data + other.data)
     out.operands = [
@@ -21,6 +36,17 @@ class Value(object):
     out.op = '+'
     return out
 
+  @scalar_to_value
+  def __sub__(self, other):
+    out = Value(self.data - other.data)
+    out.operands = [
+      Operand(self, 1),
+      Operand(self, -1)
+    ]
+    out.op = '-'
+    return out
+
+  @scalar_to_value
   def __mul__(self, other):
     out = Value(self.data * other.data)
     out.operands = [
@@ -29,6 +55,38 @@ class Value(object):
     ]
     out.op = '*'
     return out
+
+  @scalar_to_value
+  def __truediv__(self, other):
+    out = Value(self.data / other.data)
+    out.operands = [
+      Operand(self, 1 / other.data),
+      Operand(other, -self.data / (other.data ** 2))
+    ]
+    out.op = '/'
+    return out
+
+  @scalar_to_value
+  def __pow__(self, other):
+    out = Value(self.data ** other)
+    out.operands = [
+        Operand(self, other * (self.data ** (other - 1))),
+        Operand(other, out.data * math.log(self.data))
+    ]
+    out.op = '^'
+    return out
+
+  def __rmul__(self, other):
+    return self * other
+ 
+  def __radd__(self, other):
+    return self + other
+  
+  def __rsub__(self, other):
+    return Value(other) - self
+
+  def __rtruediv__(self, other):
+    return Value(other) / self
 
   def backward(self):
     self.grad = 1
