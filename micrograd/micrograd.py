e2ee7b0537fc7ab62a2ade61a2134102d199a7b4
Anuar Talipov <anuartb@gmail.com>
2024-03-17 16:31:59 +0900

    Format files.

--- micrograd/micrograd.py
+++ micrograd/micrograd.py
@@ -2,23 +2,27 @@ import math
 from dataclasses import dataclass
 import queue
 
+
 def scalar_to_value(func):
-    """All Value operations can accept scalar values.
-    
-    Casts any scalar values that are used in operations with
-    Value to Value type.
-    """
-    def wrapper(*args):
-        if isinstance(args[1], (float, int)):
-            return func(args[0], Value(args[1]))
-        else:
-            return func(*args)
-    return wrapper
+  """All Value operations can accept scalar values.
+
+  Casts any scalar values that are used in operations with
+  Value to Value type.
+  """
+
+  def wrapper(*args):
+    if isinstance(args[1], (float, int)):
+      return func(args[0], Value(args[1]))
+    else:
+      return func(*args)
+
+  return wrapper
+
 
 class Value(object):
   def __init__(self, data):
     self.data = data
-    self.operands = [] # list[Operand]
+    self.operands = []  # list[Operand]
     self.grad = 0
     self.is_enqueued = False
     self.op = ''
@@ -29,30 +33,21 @@ class Value(object):
   @scalar_to_value
   def __add__(self, other):
     out = Value(self.data + other.data)
-    out.operands = [
-        Operand(self, 1),
-        Operand(other, 1)
-    ]
+    out.operands = [Operand(self, 1), Operand(other, 1)]
     out.op = '+'
     return out
 
   @scalar_to_value
   def __sub__(self, other):
     out = Value(self.data - other.data)
-    out.operands = [
-      Operand(self, 1),
-      Operand(self, -1)
-    ]
+    out.operands = [Operand(self, 1), Operand(self, -1)]
     out.op = '-'
     return out
 
   @scalar_to_value
   def __mul__(self, other):
     out = Value(self.data * other.data)
-    out.operands = [
-        Operand(self, other.data),
-        Operand(other, self.data)
-    ]
+    out.operands = [Operand(self, other.data), Operand(other, self.data)]
     out.op = '*'
     return out
 
@@ -61,27 +56,27 @@ class Value(object):
     out = Value(self.data / other.data)
     out.operands = [
       Operand(self, 1 / other.data),
-      Operand(other, -self.data / (other.data ** 2))
+      Operand(other, -self.data / (other.data**2)),
     ]
     out.op = '/'
     return out
 
   @scalar_to_value
   def __pow__(self, other):
-    out = Value(self.data ** other)
+    out = Value(self.data**other)
     out.operands = [
-        Operand(self, other * (self.data ** (other - 1))),
-        Operand(other, out.data * math.log(self.data))
+      Operand(self, other * (self.data ** (other - 1))),
+      Operand(other, out.data * math.log(self.data)),
     ]
     out.op = '^'
     return out
 
   def __rmul__(self, other):
     return self * other
- 
+
   def __radd__(self, other):
     return self + other
-  
+
   def __rsub__(self, other):
     return Value(other) - self
 
@@ -109,6 +104,7 @@ class Value(object):
     self.is_enqueued = False
     for operand in self.operands:
       operand.val.zero_grad()
+
 
 @dataclass
 class Operand:
